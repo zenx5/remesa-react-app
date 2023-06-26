86579fcecd01d2335a79cc3288ac85e9addfb7db
Octavio Martinez <omartinez1618@gmail.com>
2023-06-26 02:24:06 -0400

    feat(App.js): add Changes and Currencies views to the back route. Remove FormBack view.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import FormFront from "./views/FormFront/Index";
-import FormBack from "./views/FormBack";
+import Changes from "./views/Changes/Index";
+import Currencies from "./views/Currencies/Index"
 
 export default function App(){
 
   return <Routes>
     <Route path="/" element={<FormFront />} />
-    <Route path="/back" element={<FormBack />} />
+    <Route path="/back/changes" element={<Changes />} />
+    <Route path="/back/currencies" element={<Currencies />} />
   </Routes>
 }
