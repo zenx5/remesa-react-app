cb8d7e733c2ac5e004533dc2464b3aa5a0180167
Octavio Martinez <omartinez1618@gmail.com>
2023-06-27 00:30:07 -0400

    feat(Wizard.js): add StepThree component and new buttons labels for each step. Add avalaibility check to enable/disable next button. Add reload button in StepThree.

--- src/views/FormFront/Wizard.js
+++ src/views/FormFront/Wizard.js
@@ -2,10 +2,24 @@ import { Box, Button, CircularProgress } from '@mui/material'
 import { useWizard } from "./Context"
 import StepOne from "./StepOne"
 import StepTwo from './StepTwo'
+import StepThree from './StepThree'
 
 
 export default function Wizard(){
-    const { step, next, back, loading } = useWizard()
+    const { step, avalaibility, next, back, loading } = useWizard()
+
+    const labelNext = [
+        'Siguiente',
+        'Guardar',
+        ' - '
+    ]
+
+    const labelBack = [
+        ' - ',
+        'Atras',
+        ' - '
+    ]
+
 
     return <>
         <Box sx={{ display:'flex', alignItems:'center', justifyContent:'center', height:'200px' }}>
@@ -14,12 +28,14 @@ export default function Wizard(){
          : <>
                 {step===1 && <StepOne /> }
                 {step===2 && <StepTwo /> }
+                {step===3 && <StepThree /> }
            </>
         }
         </Box>
         <Box sx={{ display:'flex', gap:2, mt:2 }}>
-            <Button variant="outlined" onClick={back} disabled={step===1}>Atras</Button>
-            <Button variant="outlined" onClick={next}>{ step===2?'Guardar':'Siguiente'}</Button>
+            <Button variant="outlined" sx={{ width:'150px' }} onClick={back} disabled={step===1||step===3}>{ labelBack[step-1]}</Button>
+            <Button variant="outlined" sx={{ width:'150px' }} onClick={next} disabled={step===3||!avalaibility}>{ labelNext[step-1] }</Button>
+            { step===3 && <Button variant="outlined" onClick={()=>document.location.reload()}>Nuevo</Button>}
         </Box>
     </>
 }
