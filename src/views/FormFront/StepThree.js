533aecc015b28b4096b2bb95ae42a1171864c6ec
Octavio Martinez <omartinez1618@gmail.com>
2023-06-27 00:31:01 -0400

    feat(FormFront, Operations): add new components and functionality to the app
    Add a new component called StepThree to FormFront view. This component shows a message to the user indicating that their request is being processed.
    
    Add a new component called Index to Operations view. This component shows a list of operations and allows the user to add, delete, and verify them. It also allows the user to clear all operations.
    
    Add functionality to Operations view to fetch operations and currencies from the server. Add functionality to add a new operation to the server. Add functionality to delete an operation from the server. Add functionality to verify an operation.

---
+++ src/views/FormFront/StepThree.js
@@ -0,0 +1,10 @@
+import { Stack, Typography } from '@mui/material'
+
+export default function StepThree() {
+
+    return <Stack spacing={1} sx={{width:'100%'}}>
+        <Typography>
+            Se esta procesando su solicitud, estaremos comunicandonos enseguida
+        </Typography>
+    </Stack>
+}
---
+++ src/views/Operations/Index.js
@@ -0,0 +1,103 @@
+import { Stack, Divider, List, Select, Button, Card, CardContent, CardHeader, Box, TextField, ListItem, ListItemText, Typography, MenuItem, IconButton, ButtonBase } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import OperationService from '../../services/OperationService'
+import CurrencyService from '../../services/CurrencyService'
+import { LoadingButton } from '@mui/lab'
+import { Add, Check, Remove, Delete } from '@mui/icons-material'
+
+import { handlerChange } from '../../tools/Common'
+
+export default function Index() {
+    const [ from, setFrom ] = useState('')
+    const [ to, setTo ] = useState('')
+    const [ price, setPrice ] = useState(1)
+    const [ operations, setOperations ] = useState([])
+    const [ currencies, setCurrencies ] = useState([])
+    const [ loading, setLoading ] = useState(false)
+    const [ loaded, setLoaded ] = useState(false)
+
+
+    useEffect(()=>{
+        if( !loaded ) {
+            (async ()=>{
+                setLoading(true)
+                const tempOperations = await OperationService.get()
+                const tempCurrencies = await CurrencyService.get()
+                console.log( tempOperations )
+                setOperations( prev => tempOperations )
+                setCurrencies( prev => tempCurrencies )
+                setLoaded(true)
+                setLoading(false)
+            })()
+        }
+    },[loaded, operations])
+
+    const saveChange = async () => {
+        setLoading(true)
+        const tempOperations = await OperationService.set({
+            currency_from: from,
+            currency_to: to,
+            price: price,
+            rules: []
+        })
+        setOperations( prev => tempOperations )
+        setFrom('')
+        setTo('')
+        setPrice(1)
+        setLoading(false)
+    }
+
+    const handlerDelete = (id) => async () => {
+        setLoading(true)
+        const result = await OperationService.delete({
+            id
+        })
+        setOperations( prev => result )
+        setLoading(false)
+    }
+
+    const clearAll = async () => {
+        // const response = confirm("Borrar?")
+        if( true ) {
+            setLoading(true)
+            for(const operation of operations ) {
+                await OperationService.delete({
+                    id:operation.id
+                })
+            }
+            setOperations([])
+            setLoading(false)
+        }
+    }
+
+    const handlerVerify = async (id) => {
+        setLoading(true)
+        await OperationService.set({
+            id,
+            verify:true
+        })
+        setLoading(false)
+    }
+
+    return <Card>
+        <CardHeader title="Operations" />
+        <CardContent>
+            <Button onClick={clearAll}>Borrar</Button>
+            <List>{operations.map( operation => <span key={operation.id}>
+                <ListItem secondaryAction={<>
+                        <IconButton onClick={()=>handlerVerify(operation.id)}><Check sx={{ color: operation.verify ? 'green' : 'gray' }}/></IconButton>
+                        <IconButton onClick={handlerDelete}><Delete sx={{ color: 'red' }}/></IconButton>
+                    </>}>
+                    <ListItemText 
+                        primary={<Stack spacing={1}>
+                            <Typography><b>Referencia:</b> {operation.id}</Typography>
+                            <Typography><b>Monto Depositado:</b> {operation.mount} {operation.currency_from}</Typography>
+                            <Typography><b>Monto Esperado:</b> {operation.mount*operation.price} {operation.currency_to}</Typography>
+                        </Stack>}
+                    />
+                </ListItem>
+                <Divider />
+            </span>)}</List>
+        </CardContent>
+    </Card>
+}
