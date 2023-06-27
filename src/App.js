6789b9ae4c5970e4051628a2f86ce18b2cf2e906
Octavio Martinez <omartinez1618@gmail.com>
2023-06-27 00:26:40 -0400

    feat(App.js): add new route for Operations view in the back office section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import FormFront from "./views/FormFront/Index";
 import Changes from "./views/Changes/Index";
 import Currencies from "./views/Currencies/Index"
+import Operations from "./views/Operations/Index"
 
 export default function App(){
 
@@ -9,5 +10,6 @@ export default function App(){
     <Route path="/" element={<FormFront />} />
     <Route path="/back/changes" element={<Changes />} />
     <Route path="/back/currencies" element={<Currencies />} />
+    <Route path="/back/operations" element={<Operations />} />
   </Routes>
 }
