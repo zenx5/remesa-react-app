a1c476618b701857dd10efe6d16c9fad39343c61
Octavio Martinez <omartinez1618@gmail.com>
2023-07-04 22:42:18 -0400

    try catch added

--- src/tools/Request.js
+++ src/tools/Request.js
@@ -2,13 +2,17 @@
 //sessionStorage.getItem('eu_token')
 
 export const Request = async (url, action,token, data = {}) => {
-    const response = await fetch(`${url}/${action}`, {
-        method:'post',
-        //body:`action=${action}&token=${token}${queryStringData?'&'+queryStringData:''}`
-        body:JSON.stringify({
-            token,
-            ...data
+    try{
+        const response = await fetch(`${url}/${action}`, {
+            method:'post',
+            //body:`action=${action}&token=${token}${queryStringData?'&'+queryStringData:''}`
+            body:JSON.stringify({
+                token,
+                ...data
+            })
         })
-    })
-    return await response.json()
+        return await response.json()
+    } catch( error ) {
+        return []
+    }
 }
